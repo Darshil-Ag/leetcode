f130837823af5243385d1c65f50dc83fe0d5b96e
Darshil Agarwal <darshilagarwal2005@gmail.com>
2026-06-25 15:41:11 +0530

    Time: 25 ms (35.35%) | Memory: 17 MB (24.71%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            if key not in anagrams:
+                anagrams[key] = []
+            anagrams[key].append(s)
+            
+        return list(anagrams.values())
