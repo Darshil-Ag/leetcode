631a65410f0bf40135a119f0cbbfc366cec8f31b
Darshil Agarwal <darshilagarwal2005@gmail.com>
2026-07-01 14:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.20%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Solution(object):
+    def calcEquation(self, equations, values, queries):
+        graph = defaultdict(list)
+
+        for (a, b), val in zip(equations, values):
+            graph[a].append((b, val))
+            graph[b].append((a, 1.0 / val))
+
+        def dfs(src, dst, visited):
+            if src == dst:
+                return 1.0
+
+            visited.add(src)
+
+            for nei, weight in graph[src]:
+                if nei not in visited:
+                    ans = dfs(nei, dst, visited)
+                    if ans != -1.0:
+                        return weight * ans
+
+            return -1.0
+
+        res = []
+
+        for a, b in queries:
+            if a not in graph or b not in graph:
+                res.append(-1.0)
+            else:
+                res.append(dfs(a, b, set()))
+
+        return res
